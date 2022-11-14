fefc4d44bf18ab1cb856ecffd5d27fee09ab5cd7
NikitinTimofey <115427576+NikitinTimofey@users.noreply.github.com>
2022-11-14 13:13:01 +0300

    Update exercise-1.1.cpp

--- lab1/exercise-1.1.cpp
+++ lab1/exercise-1.1.cpp
@@ -1,11 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-#include <locale.h>
 using namespace std;
 
 int main(int argc, char* argv[]) {
-	setlocale(LC_ALL, "eng");
 	float x,a;
 	printf("Enter x:");
 	scanf("%f", &x);
