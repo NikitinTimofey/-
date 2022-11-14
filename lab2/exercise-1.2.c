319209742adcff19826fb8aae0fa3d4fea06f728
NikitinTimofey <115427576+NikitinTimofey@users.noreply.github.com>
2022-11-14 13:09:42 +0300

    Update exercise-1.2.c

--- lab2/exercise-1.2.c
+++ lab2/exercise-1.2.c
@@ -1,16 +1,15 @@
-#include<stdio.h> 
+#include<stdio.h>
+#include<math.h>
+void main() {
 
-void main() { 
- 
- int r, b; 
- printf("Radius:"); 
- scanf("%d", &r); 
- printf("Hole width and height:"); 
- scanf("%d", &b); 
- printf("Is it possible to cut a square hole of this size in this circle?");
- 
- if (r>b) 
-	printf("\r\nYes"); 
- else (r<=b), 
-	printf("\r\nNo"); 
+	int r, b;
+	printf("Radius:");
+	scanf("%d", &r);
+	printf("Hole width and height:");
+	scanf("%d", &b);
+	printf("Is it possible to cut a square hole of this size in this circle?");
+	if (r*2>b*sqrt(2))
+		printf("\r\nYes");
+	else 
+		printf("\r\nNo");
 }
