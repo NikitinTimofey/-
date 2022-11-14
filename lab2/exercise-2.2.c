6e6c56c1d3caadcdd09e376d027665ab794495cd
NikitinTimofey <115427576+NikitinTimofey@users.noreply.github.com>
2022-11-14 13:11:09 +0300

    Update exercise-2.2.c

--- lab2/exercise-2.2.c
+++ lab2/exercise-2.2.c
@@ -1,10 +1,6 @@
 #include<stdio.h>
-#include<locale.h>
-
-void main()
-{
-	setlocale(LC_ALL, "Rus");
-	int a, b, c;
+void main() {
+	int a, b, c, s;
 	printf("Enter a:");
 	scanf("%i", &a);
 	printf("Enter b:");
@@ -12,12 +8,9 @@ void main()
 	printf("Enter c:");
 	scanf("%i", &c);
 	
-	if (a>0) a=1;
-		else a=0;
-	if (b>0) b=1;
-		else b=0;
-	if (c>0) c=1;
-		else c=0;
-	
-	printf("Amount of positive numbers: %i", a+b+c);
+	if (a>0) s++;
+	if (b>0) s++;
+	if (c>0) s++; 
+		
+	printf("%i", s);
 }
