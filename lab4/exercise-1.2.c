21fe32ae4b99d903af487365f14088fa545d90ac
NikitinTimofey <115427576+NikitinTimofey@users.noreply.github.com>
2022-11-27 19:49:35 +0300

    Update exercise-1.2.c

--- lab4/exercise-1.2.c
+++ lab4/exercise-1.2.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 
 int main(void) {
-	int a[] = {1,2,3,4,5,6,7,8,9,10};
+	int arr[] = {1,2,3,4,5,6,7,8,9,10};
 	for (int i=10; i>0; --i)
-	printf ("%d", a [i-1]);
-
+	printf ("%d ", arr[i-1]);
 	return 0;
 	}
