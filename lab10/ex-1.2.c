61f94dc4f3dc0ac9c1ab52b68b7586c5195dab2c
NikitinTimofey <115427576+NikitinTimofey@users.noreply.github.com>
2023-05-15 18:48:17 +0300

    Update ex-1.2.c

--- lab10/ex-1.2.c
+++ lab10/ex-1.2.c
@@ -1,79 +1,41 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main()
-{
-	int n, i, j, k;
-	double d, s;
-	
-	printf("Poryadok: \n");
-	scanf("%d", &n);
-	
-	double **a = (double **)malloc(n * sizeof(double *));
-	for (i = 0; i < n; i++)
-		a[i] = (double *)malloc(n * sizeof(double));
-	
-	double **a1 = (double **)malloc(n * sizeof(double *));
-	for (i = 0; i < n; i++)
-		a1[i] = (double *)malloc(n * sizeof(double));
-	
-	double *b = (double *)malloc(n * sizeof(double));
-	double *x = (double *)malloc(n * sizeof(double));
-	
-	printf("Vvedite koefficienty i svobodnye chleny: \n");
-	
-	for (i = 0; i < n; i++)
-	{
-		for (j = 0; j < n; j++)
-		{
-			printf("a[%d,%d]= ", i+1, j+1);
-			scanf("%lf", &a[i][j]);
-			a1[i][j] = a[i][j];
-		}
-		printf("b,[%d]= ", i+1);
-		scanf("%lf", &b[i]);
-	}
-	
-	for (k = 0; k < n; k++) // прямой ход
-	{
-		for (j = k + 1; j < n; j++)
-		{
-			d = a[j][k] / a[k][k];
-			for (i = k; i < n; i++)
-			{
-				a[j][i] = a[j][i] - d * a[k][i];
-			}
-			b[j] = b[j] - d * b[k];
-		}
-	}
-	
-	for (k = n - 1; k >= 0; k--) // обратный ход
-	{
-		d = 0;
-		for (j = k + 1; j < n; j++)
-		{
-			s = a[k][j] * x[j];
-			d = d + s;
-		}
-		x[k] = (b[k] - d) / a[k][k];
-	}
-	
-	printf("Korni sistemy: \n");
-	
-	for (i = 0; i < n; i++)
-	{
-		printf("x[%d]= %lf \n", i+1, x[i]);
-	}
-	
-	for (i = 0; i < n; i++)
-	{
-		free(a[i]);
-		free(a1[i]);
-	}
-	free(a);
-	free(a1);
-	free(b);
-	free(x);
-	
-	return 0;
+int main() {
+    int i, j, k;
+    int n = 3;
+    double arr[3][4] = {
+        {0.12, -0.43, 0.14, -0.17},
+        {-0.07, 0.34, 0.72, 0.62},
+        {1.18, -0.08, -0.25, 1.12},
+    };
+    double x[3];
+
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n + 1; j++) {
+            printf("%lf ", arr[i][j]);
+        }
+        printf("\n");
+    }
+
+    for (k = 0; k < n - 1; k++) {
+        for (j = k + 1; j < n; j++) {
+            double m = arr[j][k] / arr[k][k];
+            for (i = 0; i < n + 1; i++) {
+                arr[j][i] = arr[j][i] - m * arr[k][i];
+            }
+        }
+    }
+
+    for (i = n - 1; i >= 0; i--) {
+        x[i] = arr[i][n] / arr[i][i];
+        for (int c = n - 1; c > i; c--) {
+            x[i] = x[i] - arr[i][c] * x[c] / arr[i][i];
+        }
+    }
+
+    for (i = 0; i < n; i++) {
+        printf("x%d = %lf\n", i + 1, x[i]);
+    }
+
+    return 0;
 }
